3b039aaba3e9c834a5bebd9bdfd07c7a63679e53
Ramses Rojas <rojas.aguilar.ramses721@gmail.com>
2025-08-21 23:27:01 -0700

    increased tourSchema

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,9 +6,46 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: { type: Number, required: true, default: 4.5 },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: { type: Number, required: true, default: 4.5 },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: { type: Number, required: [true, 'A tour must have a price'] },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A image must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  startDates: [Date],
 });
 
 module.exports = mongoose.model('Tour', tourSchema);
