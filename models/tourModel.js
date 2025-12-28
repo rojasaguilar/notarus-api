9e1fddc717898f507b55c4c230b71762e094bcce
rojas <rojas.devnet@gmail.com>
2025-12-27 22:22:07 -0600

    virtual props and document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,52 +1,90 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Tour must have a name'],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: { type: Number, required: true, default: 4.5 },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: { type: Number, required: [true, 'A tour must have a price'] },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A image must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: () => Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: { type: Number, required: true, default: 4.5 },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: { type: Number, required: [true, 'A tour must have a price'] },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A image must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: () => Date.now(),
-    select: false
-  },
-  startDates: [Date],
+);
+
+//VIRTUALS
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
+
+// MIDDLEWARES
+
+//DOCUMENT MIDDLEWARE: runs before .save() and .create()
+// but not on insertMany()
+
+tourSchema.pre('save', function (next) {
+  //this points to the current processing document
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+/*
+tourSchema.pre('save', (next) => {
+  console.log('Will save document...');
+  next();
+});
+
+//POST MIDDLEWARES: runs before all pre middlewares are completed
+tourSchema.post('save', (doc, next) => {
+  console.log(doc);
+  next();
+});
+*/
 
 module.exports = mongoose.model('Tour', tourSchema);
