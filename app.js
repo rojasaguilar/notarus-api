f86143d84d328108c6478acbff1965482d7ebf72
rojas <rojas.devnet@gmail.com>
2025-12-30 23:04:51 -0600

    unhandled routes middleware

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ app.use(express.json()); //middleware to parse body (TO JSON)
 app.use(express.static(`${__dirname}/public`)); //MIDDLEWARE TO SERVE STATIC FILES
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-}); // MIDDLEWARE CUSTOM
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 }); // MIDDLEWARE TO SHOW THE TIME THE REQUEST WAS SEND
@@ -26,6 +21,15 @@ app.use((req, res, next) => {
 // 2) MIDDLEWARE FOR SPECIFIC ROUTES (MY ROUTERS)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//3) MIDDLEWARE FOR NOT HANDLED ROUTES
+
+app.all('*', (req, res, next) => {
+  return res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on the server`,
+  });
+});
 
 // EXPORTING APP SO IT CAN BE USED ON SERVER.JS
 module.exports = app;
