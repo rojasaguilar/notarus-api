2d6424a6081a2d0e48d43e41ed8d16ca0ba0d4ee
Ramses Rojas <rojas.devnet@gmail.com>
2026-01-02 16:11:42 -0700

    server.js uncaugth promises and uncaugth exceptions

--- server.js
+++ server.js
@@ -2,6 +2,17 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  //CATCHES ALL SYNC EXCEPTIONS LIKE PRINTING A VARIABLE THAT DOESN'T EXISTS...
+  console.log(err.name, err.message);
+
+  console.log('UNCAUGTH EXCEPTIONS! 🩸 Shutting down...');
+
+  //HERE WE DON'T WANT TO RESTART THE SERVER
+  //DO CRASH DE APP PURPOSELY
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE_URL.replace(
@@ -10,26 +21,39 @@ const DB = process.env.DATABASE_URL.replace(
 );
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(DB, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  await mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
 };
 
-connectDB();
+connectDB().then('MongoDB connected');
 
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 // 4) START THE SERVER
-mongoose.connection.once('open', async () => {
-  app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
-  });
+
+//ASIGN THE SERVER TO A CONST
+const server = app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
 });
+
+//CATCH UNHANDLED PROMISES
+// PROMISES WITH NO CATCH IN ALL THE APPLICATION
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+
+  console.log('UNHANDLER REJECTION! 🔥 Shutting donwn...');
+
+  //CLOSE THE SERVER AND THEN EXIT THE APP
+  //CLOSING THE SERVER WILL KILL ALL THE PENDING REQUESTS
+  server.close(() => process.exit(1));
+
+  //IS BETTER TO IMPLEMENT A MECHANISM TO RESTART THE SERVER
+});
+
+//TO INVOKE A EXCEPTION
+// console.log(x);
